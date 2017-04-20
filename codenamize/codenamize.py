d14788c354576a9163ba8dff84fde8917f52bc3e
Jose Juan Montes <jjmontes@gmail.com>
2017-04-20 03:50:50 +0200

    Fixed wrong version and algorithm list in Python 3.

--- codenamize/codenamize.py
+++ codenamize/codenamize.py
@@ -100,6 +100,7 @@ An example is shown by running  codenamize --tests .
 import argparse
 import hashlib
 import functools
+import codenamize as codenamize_module
 
 ADJECTIVES = [
     "aback","abaft","abandoned","abashed","aberrant","abhorrent","abiding","abject","ablaze","able","abnormal","aboard","aboriginal","abortive","abounding","abrasive","abrupt","absent","absorbed","absorbing","abstracted","absurd","abundant","abusive","acceptable","accessible","accidental","accurate","acid","acidic","acoustic","acrid","actually","ad hoc","adamant","adaptable","addicted","adhesive","adjoining","adorable","adventurous","afraid","aggressive","agonizing","agreeable","ahead","ajar","alcoholic","alert","alike","alive","alleged","alluring","aloof","amazing","ambiguous","ambitious","amuck","amused","amusing","ancient","angry","animated","annoyed","annoying","anxious","apathetic","aquatic","aromatic","arrogant","ashamed","aspiring","assorted","astonishing","attractive","auspicious","automatic","available","average","awake","aware","awesome","awful","axiomatic",
@@ -295,12 +296,12 @@ def main():
     parser.add_argument('--tests', dest='tests', action='store_true', help='show information and samples')
     parser.add_argument('--list_algorithms', dest='list_algorithms', action='store_true',
                         help='List the hash algorithms available')
-    parser.add_argument('--version', action='version', version='clementine-remote 1.0.0')
+    parser.add_argument('--version', action='version', version='codenamize %s' % (codenamize_module.__version__))
 
     args = parser.parse_args()
 
     if args.list_algorithms:
-        for a in hashlib.algorithms:
+        for a in hashlib.algorithms_available:
             print(a)
         return
 
